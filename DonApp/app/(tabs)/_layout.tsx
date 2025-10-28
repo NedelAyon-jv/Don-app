f883db654759dba232d71b18ff3016ed5ff7738e
Andrey Arce <andrey23arce@gmail.com>
2025-10-27 20:28:36 -0700

    Update _layout.tsx
    Iconos y ventanas actualizadas

--- DonApp/app/(tabs)/_layout.tsx
+++ DonApp/app/(tabs)/_layout.tsx
@@ -2,9 +2,9 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 
 import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -13,21 +13,39 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
+        headerShown: true,
         tabBarButton: HapticTab,
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'Inicio',
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="centros" // Este es el archivo app/(tabs)/centros.tsx
+        options={{
+          title: 'Centros', // Título para el botón del tab
+          headerTitle: 'Centros de Donación', // ¡ESTA ES LA LÍNEA QUE BUSCAS!
+          tabBarIcon: ({ color }) => (
+            <Ionicons size={28} name="location" color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Publicar',
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="add-circle" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="chats" // Corresponde al nuevo archivo app/(tabs)/chats.tsx
+        options={{
+          title: 'Chats',
+          headerTitle: 'Mensajes',
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="chatbubbles" color={color} />,
         }}
       />
       <Tabs.Screen
@@ -35,7 +53,7 @@ export default function TabLayout() {
         options={{
           title: 'Perfil',
           headerTitle: 'Mi Perfil DonApp', // Título en la parte de arriba
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="person" color={color} />,
         }}
       />
     </Tabs>

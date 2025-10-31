3f25442ac689136492c14d392ad35d0930323b1b
Christian Armenta <car_arca@hotmail.com>
2025-10-30 17:38:30 -0700

    feat: added auth middleware

---
+++ server/src/middleware/index.ts
@@ -0,0 +1,7 @@
+export * from './auth.middleware';
+export * from './cache.middleware';
+export * from './cors.middleware';
+export * from './error.middleware';
+export * from './errorHandler.middleware';
+export * from './rateLimit.middleware';
+export * from './validation.middleware';
